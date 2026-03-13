82c1b29ae67e968204dd32d347295981559e0ff6
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-03-13 16:32:47 -0300

    colocado os imports por extenso

--- kourts-api/src/main/java/br/edu/ifsc/fln/kourts/api/controller/FuncionarioController.java
+++ kourts-api/src/main/java/br/edu/ifsc/fln/kourts/api/controller/FuncionarioController.java
@@ -2,16 +2,26 @@ package br.edu.ifsc.fln.kourts.api.controller;
 
 import br.edu.ifsc.fln.kourts.api.model.domain.Funcionario;
 import br.edu.ifsc.fln.kourts.api.repository.FuncionarioRepository;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController// Declara que essa classe será um Controller
 public class FuncionarioController {
 
-    private final FuncionarioRepository funcionarioRepository;
+    private final FuncionarioRepository funcionarioRepository; // Injeção de dependecia da
+    // Interface Repository
 
     public FuncionarioController(FuncionarioRepository funcionarioRepository) {
         this.funcionarioRepository = funcionarioRepository;
