a6ea78c86187ded5fb9bb37f62fe3afe47635faf
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-03-13 16:42:33 -0300

    escrito o código teste para retornar informações da API

--- kourts-pwa/jogador/classes-api/Funcionario.inc.php
+++ kourts-pwa/jogador/classes-api/Funcionario.inc.php
@@ -1,3 +1,17 @@
 <?php
+// 1. Receber a resposta da API (ex: via cURL)
+$jsonResponse = file_get_contents('http://localhost:8081/kourts.com.br/Funcionario/152');
 
+// 2. Converter o JSON para um Objeto PHP
+$funcionario = json_decode($jsonResponse);
+
+// 3. Acessar as propriedades do objeto
+// Exibindo a foto e nao o link para a foto
+
+echo "<p> Nome: " . $funcionario->nome . "</p>";
+echo "<p> Nome: " . $funcionario->senha . "</p>"; 
+echo "<p> id: " . $funcionario->sobrenome . "</p>"; 
+echo "<p> senha: ". $funcionario->email . "</p>"; 
+echo "<p> telefone: ". $funcionario->cpf . "</p>"; 
+echo "<p> cpf: ". $funcionario->telefone . "</p>"; 
 ?>
