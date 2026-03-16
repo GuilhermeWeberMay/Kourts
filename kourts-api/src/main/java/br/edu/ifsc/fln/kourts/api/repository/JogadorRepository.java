3125185e6e6c0cce820f64c0920fc6ee95ea8062
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-03-16 20:18:20 -0300

    Feito método especial para procurar o Jogador pelo apelido

--- kourts-api/src/main/java/br/edu/ifsc/fln/kourts/api/repository/JogadorRepository.java
+++ kourts-api/src/main/java/br/edu/ifsc/fln/kourts/api/repository/JogadorRepository.java
@@ -1,8 +1,11 @@
 package br.edu.ifsc.fln.kourts.api.repository;
 
+import br.edu.ifsc.fln.kourts.api.model.domain.Funcionario;
 import br.edu.ifsc.fln.kourts.api.model.domain.Jogador;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JogadorRepository extends JpaRepository<Jogador,Integer> {
+    // Método feito para pesquisar funcionário por CPF
+    Jogador findByApelido(String apelido);
 }
