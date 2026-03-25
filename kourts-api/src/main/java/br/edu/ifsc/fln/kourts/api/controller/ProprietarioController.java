15c7e1b5c925858b19a300eee06c6de381d5cda8
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-03-25 16:06:07 -0300

    Organizado os imports

--- kourts-api/src/main/java/br/edu/ifsc/fln/kourts/api/controller/ProprietarioController.java
+++ kourts-api/src/main/java/br/edu/ifsc/fln/kourts/api/controller/ProprietarioController.java
@@ -1,13 +1,17 @@
 package br.edu.ifsc.fln.kourts.api.controller;
 
-import br.edu.ifsc.fln.kourts.api.model.domain.Funcionario;
-import br.edu.ifsc.fln.kourts.api.model.domain.Jogador;
 import br.edu.ifsc.fln.kourts.api.model.domain.Proprietario;
 import br.edu.ifsc.fln.kourts.api.repository.ProprietarioRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
