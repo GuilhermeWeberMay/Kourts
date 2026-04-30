14ec48edeee79561b6d6d7d212aeef9fde976f4a
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-04-30 14:37:29 -0300

    Dados basicos das quadras
    Feito a função básica para mostrar as quadras, feito no html sem a formatação

--- kourts-pwa/testeAxios/script.js
+++ kourts-pwa/testeAxios/script.js
@@ -8,26 +8,22 @@ async function mostrarQuadras() {
 
   divList.innerHTML = ``;
   quadras.map((e) => {
-    let horariosHTML = '';
-      for (const [data, horarios] of Object.entries(e.horariosDisponiveis || {})) {
-        horariosHTML += `<strong>${data}:</strong> ${horarios.join(', ')}<br>`;
-      }
+   let horariosHTML = '';
+   for (const [data, horarios] of Object.entries(e.horariosDisponiveis || {})) {
+    horariosHTML += `<strong>${data}:</strong> ${horarios.join(', ')}<br>`;
+   }
    const divQuadra = document.createElement("div");
-   divQuadra.innerHTML = ` ${e.nome}
-                        ${e.esporte}
+   divQuadra.innerHTML = ` ${e.nome}<br>
                       R$${e.precoPorHora}
-                        ${e.qtdJogadores}
-                        ${e.horaAbertura}
-                        ${horariosHTML}
-                        ${e.cep}<br>`;
-                        e.fotos.forEach(fotos => {
-                         const img = document.createElement("img");
-                         img.src = "http://localhost:8081/fotos/"+fotos
-                         divQuadra.appendChild(img)
-                        });
+                        <br>`;
+   e.fotos.forEach(fotos => {
+    const img = document.createElement("img");
+    img.src = "http://localhost:8081/fotos/" + fotos
+    divQuadra.appendChild(img)
+   });
 
    return divList.appendChild(divQuadra);
-  
+
   });
   console.log(quadras);
  } catch (Error) {
