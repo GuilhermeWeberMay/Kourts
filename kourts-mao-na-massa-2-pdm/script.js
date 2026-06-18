e89f6be42e3da9197f44fe55f4773b74b5e55391
dalmaso-Dev <pauloricardodalmaso@gmail.com>
2026-06-18 17:17:15 -0300

    fit - implementação de service worker

--- kourts-mao-na-massa-2-pdm/script.js
+++ kourts-mao-na-massa-2-pdm/script.js
@@ -459,3 +459,18 @@ function ativarToast() {
   }
 }
 
+if ('serviceWorker' in navigator) {  
+    navigator.serviceWorker.register("./service-worker.js");
+}
+
+var pedidoInstalacao;
+window.addEventListener('beforeinstallprompt', function(installPrompt) {
+    if(installPrompt){
+        document.getElementById("installAppBt").classList.remove('collapse')
+        pedidoInstalacao = installPrompt
+    }
+});
+
+function installApp() {
+    pedidoInstalacao.prompt(); 
+}
