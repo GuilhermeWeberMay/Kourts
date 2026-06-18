d286e22a5ec231974a963b2cb6fee780398e2c51
Gustavo Rockembach <rockembach.dev@gmail.com>
2026-06-18 14:33:09 -0300

    feito a tela de cadastro

---
+++ kourts-pwa/loginCadastro/script.js
@@ -0,0 +1,37 @@
+ 
+  document.addEventListener('DOMContentLoaded', () => {
+   mudaBt('login');
+  });
+
+  //troca de aba
+  function mudaBt(tela){
+    const formularios = document.getElementsByClassName('formulario');
+    Array.from(formularios).forEach(f => f.classList.remove('show'));
+
+    const btnLogin    = document.getElementById('btn-login');
+    const btnCadastro = document.getElementById('btn-cadastro');
+
+    if(tela === 'login')
+    {
+     document.getElementById('formulario-login').classList.add('show');
+     btnLogin.classList.add('ativo');
+     btnCadastro.classList.remove('ativo');
+    }
+    else {
+      document.getElementById('formulario-cadastro').classList.add('show');
+      btnCadastro.classList.add('ativo');
+      btnLogin.classList.remove('ativo');
+    }
+
+    limparErros();
+  }
+  
+/*  function redirecionarPorRole(role){
+    if(role === 'proprietario')
+    {
+     window.location.href = '../proprietario/spa/telaReservas.html#';
+    }
+    else{
+     window.location.href = '../jogador/spa/index.html#';
+    }
+  }*/
